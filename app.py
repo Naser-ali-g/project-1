963f78eb365c2387474277a213b10e3725277d46
naser ali <studnt2019@gmail.com>
2026-01-04 22:38:55 +0200

    adding the app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+PORT = int(os.environ.get('PORT', 5000))
+
+@app.route('/')
+def hello_world():
+    return 'Hello World from DevOps Pipeline!'
+
+@app.route('/health')
+def health():
+    return jsonify({'status': 'healthy'}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT, debug=False)
